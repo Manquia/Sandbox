803e43791d987a50d6cacb457961760e852a54d4
Micah Rust <micahtim@gmail.com>
2017-11-13 16:58:01 -0800

    Added some changes

--- DigiPen/CS330/ClosestPair/closest_pair_dc-files/closestpair.cpp
+++ DigiPen/CS330/ClosestPair/closest_pair_dc-files/closestpair.cpp
@@ -21,33 +21,56 @@ float distanceBetweenSq(const Point& p1, const Point& p2)
 	return vec.x * vec.x + vec.y + vec.y
 }
 
-////////////////////////////////////////////////////////////////////////////////
-float closestPair_aux ( .................... );
+struct SortByX
+{
+	bool operator()(const Point& left, const Point& right)
+	{
+		return left.x < right.x;
+	}
+}
+struct SortByY
+{
+	bool operator()(const Point& left, const Point& right)
+	{
+		return left.y < right.y;
+	}
+	
+}
 
+////////////////////////////////////////////////////////////////////////////////
+float closestPair_Split (std::vector<Point>::iterator begin, std::vector::iterator end, bool verticalDivide, float& closestPairSq);
+
+float closestPair_Brute(std::
 ////////////////////////////////////////////////////////////////////////////////
 float closestPair ( std::vector< Point > const& points ) {
 	int size = points.size();
 
-	//std::cerr << "closestPair_aux " << size << " points:";
+	// copy points so that we can sort and modify them
+	std::vector<Point> givenPoints = points;
+	
+	//std::cerr << "closestPair_Split " << size << " points:";
 	//for(int i=0;i<size;++i) { std::cerr << points[i] << " "; } std::cerr << std::endl;
 
 	if (size==0) throw "zero size subset";
 	if (size==1) return std::numeric_limits<float>::max();
 
-	return closestPair_aux( indices, points );
+	float distance = std::numeric_limits<float>::max();
+	std::sort(givenPoints.beging(), givenPoints.end(), SortByX());
+	
+	return closestPair_Split(givenPoints.begin(), givenPoints.end(),  , points );
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-float closestPair_aux ( ..................... ) {
+float closestPair_Split (std::vector<Point>::iterator begin, std::vector::iterator end, bool verticalDivide)
+{
 	int size = indices.size();
 
-	//std::cerr << "closestPair_aux ";
+	//std::cerr << "closestPair_Split ";
 	//for(int i=0;i<size;++i) { std::cerr << points[ indices[i] ] << " "; } std::cerr << std::endl;
 
 	if (size==0) throw "zero size subset";
 	if (size==1) return std::numeric_limits<float>::max();
 
-	.................
 
 	return min_dist;
 }
