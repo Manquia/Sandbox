23fac9565e259449da39e1604e7c8f142a604cb3
Manquia <micahtim@gmail.com>
2018-04-23 18:46:56 -0700

    Small touches

---
+++ DigiPen/MAT340/Coding4/Assets/Scripts/Rotator.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour {
+
+
+    public Vector3 axisNormal = Vector3.up;
+    public float speed = 2.5f;
+    
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        var rigid = GetComponent<Rigidbody>();
+        rigid.angularVelocity = axisNormal.normalized * speed;
+	}
+}
